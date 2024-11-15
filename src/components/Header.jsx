601c719ca98fc2a5174df80dee192fd43db0f141
Hamed <hamedroaddark@gmail.com>
2024-11-15 09:32:51 +0330

    Test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="mr-4 bg-slate-200 px-10 pt-8">
       <h1 className="reversed-text font-title text-center text-3xl font-bold text-blue-500">
-        Todo List App
+        Todo List App Optimization
       </h1>
     </header>
   );
